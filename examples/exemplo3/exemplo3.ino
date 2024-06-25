96a32b4ba1474f6282b1ed8b8d35a716f44f7a87
izyino <info@izyino.com.br>
2024-06-25 13:05:09 -0300

    Update exemplo3.ino

--- examples/exemplo3/exemplo3.ino
+++ examples/exemplo3/exemplo3.ino
@@ -8,7 +8,7 @@
 // em resposta, o programa exibe uma tela contendo a lista de redes
 // WiFi ao alcance, para que o usuário informe qual rede será utilizada
 // juntamente com a senha correspondente. Dependendo do número de redes
-// ao alcance, pode demorar vários minutos até que a lista seja exibida
+// ao alcance, pode demorar um certo tempo até que a lista seja exibida
 //
 // Informar então o número da rede escolhida e a sua senha, seguido por
 // um clique no botão "submit". Após, basta apontar o browser para o IP 
@@ -37,7 +37,7 @@ String p2="<tr><td align='center' bgcolor='#FFCCFF' width='100'>#AAA</td><td ali
 String p3="<tr><td colspan='4' align='center'></td></tr><form action='/ok'><tr><td colspan='4' align='center' bgcolor='#CCFF99'><b><font size='2'>REDE ESCOLHIDA</font></b></td></tr><tr><td align='center' bgcolor='#FFFF99'><b><font size='2'>nº</font></b></td><td colspan='2' align='center' bgcolor='#FFFF99'><font size='2'><b>senha</b></font></td><td align='center' rowspan='2' bgcolor='#FFCC66'><font size='2'><input type='submit' value='Submit'></font></td></tr><tr><td align='center' bgcolor='#CCFFFF'><input type='text' id='rede' name='rede' size='7'></td><td colspan='2' align='center' bgcolor='#CCFFFF'><input type='text' id='senha' name='senha' size='40'></td></tr></form></table></body></html>";
 String savP2=p2;
 
-String html="<html><head><meta charset='utf-8'><title></title></head><body><table border='0' width='600' id='table1' cellspacing='4' cellpadding='4' height='400'><tr><td colspan='6' align='center'><p align='center'><b><font size='4'>CONTROLE DO MOTOR DE PASSO<br>PARA TESTE DA PCI_UG_T1 VIA Wi-Fi</font></b></p></td></tr><tr><td colspan='6' align='center'>&nbsp;</td></tr><tr><td colspan='6' align='center' bgcolor='#FFCCFF'><font size='2'><b>VELOCIDADE</b></font></td></tr><tr><td align='center' bgcolor='#CV3' width='100'><a href='/V3'><font size='4'>&nbsp;&nbsp;3&nbsp;&nbsp;</font></td></a><td align='center' bgcolor='#CV5' width='100'><a href='/V5'><font size='4'>&nbsp;&nbsp;5&nbsp;&nbsp;</font></td></a><td align='center' bgcolor='#CV10' width='100'><a href='/V10'><font size='4'>&nbsp;&nbsp;10&nbsp;&nbsp;</font></td></a><td align='center' bgcolor='#CV20' width='100'><a href='/V20'><font size='4'>&nbsp;&nbsp;20&nbsp;&nbsp;</font></td></a><td align='center' bgcolor='#CV40' width='100'><a href='/V40'><font size='4'>&nbsp;&nbsp;40&nbsp;&nbsp;</font></td></a><td align='center' bgcolor='#CV80' width='100'><a href='/V80'><font size='4'>&nbsp;&nbsp;80&nbsp;&nbsp;</font></td></a></tr><tr><td colspan='6' align='center'>&nbsp;</td></tr><tr><td colspan='6' align='center' bgcolor='#CCFF99'><p align='center'><font size='2'><b>DIREÇÃO</b></font></td></tr><tr><td colspan='3' align='center' bgcolor='#CDE' width='300'><a href='/DE'><font size='4'>&nbsp;&nbsp;&nbsp;&nbsp;ESQUERDA&nbsp;&nbsp;&nbsp;&nbsp;</font></td></a><td colspan='3' align='center' bgcolor='#CDD' width='300'><a href='/DD'><font size='4'>&nbsp;&nbsp;&nbsp;&nbsp;DIREITA&nbsp;&nbsp;&nbsp;&nbsp;</font></td></a></tr><tr><td colspan='6' align='center'>&nbsp;</td></tr><tr><td colspan='6' align='center' bgcolor='#99CCFF'><p align='center'><font size='2'><b>MOVIMENTO</b></font></td></tr><tr><td align='center' bgcolor='#C15' width='100'><a href='/C15'><font size='4'>&nbsp;15º&nbsp;</font></td></a><td align='center' bgcolor='#C30' width='100'><a href='/C30'><font size='4'>&nbsp;30º&nbsp;</font></td></a><td align='center' bgcolor='#C45' width='100'><a href='/C45'><font size='4'>&nbsp;45º&nbsp;</font></td></a><td align='center' bgcolor='#C90' width='100'><a href='/C90'><font size='4'>&nbsp;90º&nbsp;</font></td></a><td align='center' bgcolor='#CCP' width='100'><a href='/CCP'><font size='2'>ANDA<br>PARA</font></td></a><td align='center' bgcolor='#1VS' width='100'><a href='/1VS'><font size='2'>UMA DOSE<br>VIA SENSOR</font></td></a></tr></table></body></html>";
+String html="<html><head><meta charset='utf-8'><title></title></head><body><table border='0' width='600' id='table1' cellspacing='4' cellpadding='4' height='400'><tr><td colspan='6' align='center'><p align='center'><b><font size='4'>CONTROLE DO MOTOR DE PASSO<br>PARA TESTE DA PCI_UG_T1 VIA Wi-Fi</font></b></p></td></tr><tr><td colspan='6' align='center'>&nbsp;</td></tr><tr><td colspan='6' align='center' bgcolor='#FFCCFF'><font size='2'><b>VELOCIDADE</b></font></td></tr><tr><td align='center' bgcolor='#CV3' width='100'><a href='/V3'><font size='4'>&nbsp;&nbsp;3&nbsp;&nbsp;</font></td></a><td align='center' bgcolor='#CV5' width='100'><a href='/V5'><font size='4'>&nbsp;&nbsp;5&nbsp;&nbsp;</font></td></a><td align='center' bgcolor='#CV10' width='100'><a href='/V10'><font size='4'>&nbsp;&nbsp;10&nbsp;&nbsp;</font></td></a><td align='center' bgcolor='#CV20' width='100'><a href='/V20'><font size='4'>&nbsp;&nbsp;20&nbsp;&nbsp;</font></td></a><td align='center' bgcolor='#CV40' width='100'><a href='/V40'><font size='4'>&nbsp;&nbsp;40&nbsp;&nbsp;</font></td></a><td align='center' bgcolor='#CV80' width='100'><a href='/V80'><font size='4'>&nbsp;&nbsp;80&nbsp;&nbsp;</font></td></a></tr><tr><td colspan='6' align='center'>&nbsp;</td></tr><tr><td colspan='6' align='center' bgcolor='#CCFF99'><p align='center'><font size='2'><b>DIREÇÃO</b></font></td></tr><tr><td colspan='3' align='center' bgcolor='#CDE' width='300'><a href='/DE'><font size='4'>&nbsp;&nbsp;&nbsp;&nbsp;ESQUERDA&nbsp;&nbsp;&nbsp;&nbsp;</font></td></a><td colspan='3' align='center' bgcolor='#CDD' width='300'><a href='/DD'><font size='4'>&nbsp;&nbsp;&nbsp;&nbsp;DIREITA&nbsp;&nbsp;&nbsp;&nbsp;</font></td></a></tr><tr><td colspan='6' align='center'>&nbsp;</td></tr><tr><td colspan='6' align='center' bgcolor='#99CCFF'><p align='center'><font size='2'><b>MOVIMENTO</b></font></td></tr><tr><td align='center' bgcolor='#C15' width='100'><a href='/C15'><font size='4'>&nbsp;15º&nbsp;</font></td></a><td align='center' bgcolor='#C30' width='100'><a href='/C30'><font size='4'>&nbsp;30º&nbsp;</font></td></a><td align='center' bgcolor='#C45' width='100'><a href='/C45'><font size='4'>&nbsp;45º&nbsp;</font></td></a><td align='center' bgcolor='#C90' width='100'><a href='/C90'><font size='4'>&nbsp;90º&nbsp;</font></td></a><td align='center' bgcolor='#CCP' width='100'><a href='/CCP'><font size='2'>ANDA<br>PARA</font></td></a><td align='center' bgcolor='#1VS' width='100'><a href='/1VS'><font size='2'>UMA DOSE<br>VIA SENSOR</font></td></a></tr><tr><td colspan='6' align='center'><p align='center'><br><a href='/ini'>INIFLASH</a><br>Depois de clicar em INIFLASH, conectar na rede PCI_UG_T1, senha 00000000<br> Acessar 192.168.4.1 para escolher a rede a ser utilizada</p></td></tr></table></body></html>";
 String copyhtml;
 
 uint32_t nsteps;   //steps a percorrer
@@ -53,7 +53,9 @@ void setup() {
   Serial.begin(115200);
   EEPROM.begin(128);
   pinMode(32, INPUT);
-  delay(1000);
+  x.begin();
+  x.bdur=200;x.bfreq=2000;x.binter=0;x.bnum=1;
+  x.ldur=50;x.linter=25;x.lnum=10;
 
   dumpflash();  //mostra o estado atual dos 128 bytes da memória flash  
 
@@ -92,7 +94,6 @@ void setup() {
   Serial.print("Vcc no momento (5vcc): ");Serial.print(float(analogRead(32))/3103.0*5.0,1);Serial.println(" volts");
   Serial.println("-----------------------------------------");
   server.begin();
-  x.begin();
 }
 
 void loop(){
@@ -111,9 +112,12 @@ void loop(){
             client.println("Connection: close");
             client.println();
 
-            //quando recebe um comando do browser, emite um beep de 0.2s e pisca o led 10 vezes
-            x.bdur=200;x.bfreq=2000;x.binter=0;x.bnum=1;
-            x.ldur=50;x.linter=25;x.lnum=10;
+            //quando recebe um comando do browser (diferente de /ini),
+            //emite um beep de 0.2s e pisca o led 10 vezes
+            if (header.indexOf("GET /ini") != 0) {
+              x.bdur=200;x.bfreq=2000;x.binter=0;x.bnum=1;
+              x.ldur=50;x.linter=25;x.lnum=10;
+            }
             
             //
             // atualiza as variáveis vel, sent e graus
@@ -136,6 +140,13 @@ void loop(){
             if (header.indexOf("GET /C90") >= 0) {graus=90;}
             if (header.indexOf("GET /CCP") >= 0) {if (graus==999){graus=0;}else{graus=999;}}
             if (header.indexOf("GET /1VS") >= 0) {graus=555;}
+
+            if (header.indexOf("GET /ini") >= 0) {
+              for (k=0; k<128; k++){EEPROM.write(k, 0x00);};EEPROM.commit();
+              x.xtime[0]=0;x.xtime[1]=0;x.xtime[2]=0;x.xtime[3]=0;
+              x.xsteps[0]=0;x.xsteps[1]=0;x.bnum=0;x.lnum=0;
+              delay(1000);ESP.restart();
+            }
 
             copyhtml=html;
 
@@ -299,6 +310,10 @@ void looprede()
               for (k=0; k<EEPROM.read(125); k++){password=password+char(EEPROM.read(k+32));}
           
               fim=true;
+
+              x.xtime[0]=0;x.xtime[1]=0;x.xtime[2]=0;x.xtime[3]=0;
+              x.xsteps[0]=0;x.xsteps[1]=0;x.bnum=0;x.lnum=0;
+              delay(1000);ESP.restart();
             }  
 
             currentLine = "";
